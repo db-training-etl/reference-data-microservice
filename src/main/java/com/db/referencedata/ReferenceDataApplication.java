717070fabcd476864c9415581c1643e38d9cbbca
Jordi Gómez <gnugomez@gmail.com>
2022-10-13 11:54:19 +0200

    chore: initial project setup

---
+++ src/main/java/com/db/referencedata/ReferenceDataApplication.java
@@ -0,0 +1,13 @@
+package com.db.referencedata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReferenceDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReferenceDataApplication.class, args);
+    }
+
+}
