4ba2e0eb73d7c5fbce0199458667ee5fcae50c2d
loui-gft <109503966+loui-gft@users.noreply.github.com>
2022-10-17 11:18:20 +0000

    Add methods to counterparty service

--- src/main/java/com/db/referencedata/controller/CounterpartyController.java
+++ src/main/java/com/db/referencedata/controller/CounterpartyController.java
@@ -1,6 +1,8 @@
 package com.db.referencedata.controller;
 
+import com.db.referencedata.entity.Counterparty;
 import com.db.referencedata.service.CounterpartyService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +12,10 @@ public class CounterpartyController {
 
     public CounterpartyController(CounterpartyService counterpartyService) {
         this.counterpartyService = counterpartyService;
+    }
+
+    @GetMapping("api/listCounterparty")
+    public Iterable<Counterparty> list(){
+        return counterpartyService.list();
     }
 }
--- src/main/java/com/db/referencedata/service/CounterpartyService.java
+++ src/main/java/com/db/referencedata/service/CounterpartyService.java
@@ -4,14 +4,26 @@ import com.db.referencedata.entity.Counterparty;
 import com.db.referencedata.repository.CounterpartyRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class CounterpartyService {
 
     CounterpartyRepository counterpartyRepository;
 
+    public CounterpartyService(CounterpartyRepository counterpartyRepository) {
+        this.counterpartyRepository = counterpartyRepository;
+    }
 
-    public String save(Counterparty counterparty) {
-        counterpartyRepository.save(counterparty);
-        return "";
+    public Iterable<Counterparty> list() {
+        return counterpartyRepository.findAll();
+    }
+
+    public Counterparty save(Counterparty counterparty) {
+        return counterpartyRepository.save(counterparty);
+    }
+
+    public Iterable<Counterparty> saveAll(List<Counterparty> counterparties) {
+        return counterpartyRepository.saveAll(counterparties);
     }
 }
