99047ab3497e4be766f0c11e26de30f46f40ae37
Judith <76495687+Jubaal@users.noreply.github.com>
2022-10-25 15:20:55 +0000

    added some exceptions in service
    added validator, exceptions in service and controller.

--- src/main/java/com/db/referencedata/controller/CounterpartyController.java
+++ src/main/java/com/db/referencedata/controller/CounterpartyController.java
@@ -5,11 +5,18 @@ import com.db.referencedata.exception.NoValuesFoundException;
 import com.db.referencedata.service.CounterpartyService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
+@Validated
 @RestController
 @RequestMapping("counterparties")
 public class CounterpartyController {
@@ -31,14 +38,31 @@ public class CounterpartyController {
     }
 
     @PutMapping("")
-    public ResponseEntity<Counterparty> save(@RequestBody Counterparty counterparty) {
+    public ResponseEntity<Counterparty> save(@Valid @RequestBody Counterparty counterparty){
         counterpartyService.save(counterparty);
         return new ResponseEntity<Counterparty>(counterparty, HttpStatus.OK);
     }
 
     @PutMapping("bulk")
-    public ResponseEntity<List<Counterparty>> saveAll(@RequestBody List<Counterparty> counterparties) {
+    public ResponseEntity<List<Counterparty>> saveAll(
+            @RequestBody
+            List<@Valid Counterparty> counterparties) throws NoValuesFoundException {
         counterpartyService.saveAll(counterparties);
         return new ResponseEntity<List<Counterparty>>(counterparties, HttpStatus.OK);
+    }
+
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity handle(ConstraintViolationException constraintViolationException) {
+        Set<ConstraintViolation<?>> violations = constraintViolationException.getConstraintViolations();
+        String errorMessage = "";
+        if (!violations.isEmpty()) {
+            StringBuilder builder = new StringBuilder();
+            violations.forEach(violation -> builder.append(" " + violation.getMessage()));
+            errorMessage = builder.toString();
+        } else {
+            errorMessage = "ConstraintViolationException occured.";
+        }
+        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/com/db/referencedata/service/CounterpartyService.java
+++ src/main/java/com/db/referencedata/service/CounterpartyService.java
@@ -3,8 +3,11 @@ package com.db.referencedata.service;
 import com.db.referencedata.entity.Counterparty;
 import com.db.referencedata.exception.NoValuesFoundException;
 import com.db.referencedata.repository.CounterpartyRepository;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
@@ -13,6 +16,7 @@ import java.util.Optional;
 public class CounterpartyService {
 
     CounterpartyRepository counterpartyRepository;
+    ExceptionSenderService exceptionSenderService;
 
     public CounterpartyService(CounterpartyRepository counterpartyRepository) {
         this.counterpartyRepository = counterpartyRepository;
@@ -20,21 +24,30 @@ public class CounterpartyService {
 
     public Counterparty findById(int id) throws Exception {
         return counterpartyRepository.findById(id).orElseThrow(() -> new Exception("Counterparty not found: " + id));
+        //call function in exception service to send to Exception Api
     }
 
     public List<Counterparty> findAll() throws NoValuesFoundException{
         if(counterpartyRepository.findAll().isEmpty())
             throw new NoValuesFoundException("List of counterparties is empty");
+            //call function in exception service to send to Exception Api
         else{
             return counterpartyRepository.findAll();
         }
     }
 
-    public Counterparty save(Counterparty counterparty) {
+    public Counterparty save(Counterparty counterparty){
         return counterpartyRepository.save(counterparty);
     }
 
-    public List<Counterparty> saveAll(List<Counterparty> counterparties) {
+    public List<Counterparty> saveAll(List<Counterparty> counterparties) throws NoValuesFoundException {
+        if(counterpartyRepository.saveAll(counterparties).isEmpty()){
+            throw new NoValuesFoundException("List of counterparties is empty");
+        }
         return counterpartyRepository.saveAll(counterparties);
     }
+
+/*    public void sendExceptionToService(String name,String type, String message,String trace, Date cobDate){
+        exceptionSenderService.sendException(name,type,message,trace,cobDate);
+    }*/
 }
